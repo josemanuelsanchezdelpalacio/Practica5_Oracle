74fef42c79ed55ca8b87b3ee98ac8c31517e7beb
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2024-01-11 12:15:05 +0100

    Initial commit

---
+++ src/main/java/Conexion/ConexionOracle.java
@@ -0,0 +1,27 @@
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionOracle {
+
+    private static final String URL = "jdbc:oracle:thin:@//localhost:1523";
+    private static final String USUARIO = "root";
+    private static final String CLAVE = "admin";
+
+    public static Connection conectar(String nombreBD) {
+        Connection conexion = null;
+        try {
+            conexion = DriverManager.getConnection(URL + "/" + nombreBD, USUARIO, CLAVE);
+            System.out.println("Conexión OK.");
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión");
+            e.printStackTrace();
+        }
+        return conexion;
+    }
+}
+
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+import Conexion.PoolConexiones;
+import code.Insertar;
+import code.Listar;
+import libs.Leer;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean salir = false;
+        int opcion;
+        Connection conexion = null;
+
+        do {
+            System.out.println("0. Salir");
+            System.out.println("1. Conectar a la base de datos");
+            System.out.println("2. Insertar alumno");
+            System.out.println("3. Listar nota media alumnos");
+
+            opcion = Leer.pedirEntero("Introduce una opción: ");
+
+            switch (opcion) {
+                case 0:
+                    salir = true;
+                    break;
+                case 1:
+                    //String nombreBD = Leer.pedirCadena("Introduce el nombre de la base de datos: ");
+                    try {
+                        PoolConexiones.conectar();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case 2:
+                    Insertar.insertarAlumno();
+                case 3:
+                    Listar.notasMediasAlumnos();
+                default:
+                    System.out.println("La opción seleccionada no existe");
+            }
+
+        } while (!salir);
+    }
+}
